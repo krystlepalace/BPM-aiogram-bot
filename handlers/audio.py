f6d00b94a691f17ca13f1f326343fa551789e41f
Fvbvke <novoselcevila12@gmail.com>
2023-09-04 02:38:29 +0300

    Updated template. Now it's working handler

--- handlers/audio.py
+++ handlers/audio.py
@@ -1,5 +1,8 @@
 from aiogram import Router, F
 from aiogram.types import Message
+from pathlib import Path
+from utils.detect_bpm import detect
+import main
 
 
 router = Router()
@@ -7,5 +10,14 @@ router = Router()
 
 @router.message(F.audio)
 async def detect_bpm_reply(message: Message):
-    await message.reply("BPM: ")
+    file_id = message.audio.file_id
+
+    file = await main.bot.get_file(file_id)
+    file_path = file.file_path
+    file_on_disk = Path(f"../meida/{file_id}.mp3")
+    
+    await main.bot.download_file(file_path, destination=file_on_disk)
+
+    bpm = detect(file_on_disk.__str__())
+    await message.reply(f"Song: {message.audio.title}\nBPM: {bpm}")
 
