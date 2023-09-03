0a94b769f27725a69615f5490c522c53fdc048a1
Fvbvke <novoselcevila12@gmail.com>
2023-09-04 02:49:23 +0300

    Infinite disk space filled with audios incindent fixed

--- handlers/audio.py
+++ handlers/audio.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from utils.detect_bpm import detect
 import main
 from config import CONFIG
+import os
 
 
 router = Router()
@@ -20,5 +21,7 @@ async def detect_bpm_reply(message: Message):
     await main.bot.download_file(file_path, destination=file_on_disk)
 
     bpm = await detect(file_on_disk.__str__())
+    os.remove(file_on_disk)
+
     await message.reply(f"Song: {message.audio.title}\nBPM: {round(bpm[-1], 3)}")
 
