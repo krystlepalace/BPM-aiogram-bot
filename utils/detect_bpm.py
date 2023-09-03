a047af114190aaf2a13a4dbe6fd80fcc35aaee5c
Fvbvke <novoselcevila12@gmail.com>
2023-09-04 02:31:43 +0300

    Created function that detects bpm from audiofile downloaded on disk

---
+++ utils/detect_bpm.py
@@ -0,0 +1,9 @@
+import librosa
+
+async def detect(filepath: str):
+    y, sr = librosa.load(filepath, sr=None)
+    bpm = 0
+    if y.size != 0:
+        bpm = librosa.beat.tempo(y=y, sr=sr)
+
+    return bpm
