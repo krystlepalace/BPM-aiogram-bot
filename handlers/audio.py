b227e5d7c0c1c820eb7785ed57e81562c6642b7c
Fvbvke <novoselcevila12@gmail.com>
2023-09-04 02:42:38 +0300

    Bug fixes

--- handlers/audio.py
+++ handlers/audio.py
@@ -19,6 +19,6 @@ async def detect_bpm_reply(message: Message):
     
     await main.bot.download_file(file_path, destination=file_on_disk)
 
-    bpm = detect(file_on_disk.__str__())
+    bpm = await detect(file_on_disk.__str__())
     await message.reply(f"Song: {message.audio.title}\nBPM: {bpm}")
 
