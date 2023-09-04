82a5bd44b0b0f14822fb32e946d0a90b65515d96
Fvbvke <novoselcevila12@gmail.com>
2023-09-04 23:12:54 +0300

    Refactor

--- handlers/audio.py
+++ handlers/audio.py
@@ -1,7 +1,7 @@
 from aiogram import Router, F
 from aiogram.types import Message
 from pathlib import Path
-from utils.detect_bpm import detect
+from utils.detecter import detect_bpm
 import main
 from config import CONFIG
 import os
@@ -21,7 +21,7 @@ async def detect_bpm_reply(message: Message):
     
     await main.bot.download_file(file_path, destination=file_on_disk)
 
-    bpm = await detect(file_on_disk.__str__())
+    bpm = await detect_bpm(file_on_disk.__str__())
     os.remove(file_on_disk)
 
     await message.reply(f"Song: {message.audio.title}\nBPM: {round(bpm[-1], 3)}\n\nBy @bpm_detect_bot")
