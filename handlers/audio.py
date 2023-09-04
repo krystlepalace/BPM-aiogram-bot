4b02b51edcb61eb7a2b0a80583c44e9efedda2c3
Fvbvke <novoselcevila12@gmail.com>
2023-09-05 00:25:30 +0300

    Fixes

--- handlers/audio.py
+++ handlers/audio.py
@@ -17,13 +17,13 @@ async def detect_bpm_reply(message: Message):
 
     file = await main.bot.get_file(file_id)
     file_path = file.file_path
-    file_on_disk = Path(f"{CONFIG.media_full_path}{file_id}.mp3")
+    file_on_disk = Path(f"{CONFIG.media_full_path}{file_id}.{file_path.split('.')[-1]}")
     
     await main.bot.download_file(file_path, destination=file_on_disk)
 
     bpm = await detect_bpm(file_on_disk.__str__())
     key = await detect_key(file_on_disk.__str__())
-    os.remove(file_on_disk)
 
     await message.reply(f"Song: {message.audio.title}\nBPM: {round(bpm[-1], 3)}\nKey: {key}\n\nBy @bpm_detect_bot")
-
+    os.remove(file_on_disk)
+    os.remove(Path(f"{file_on_disk.__str__().split('.')[0]}.mp3"))
