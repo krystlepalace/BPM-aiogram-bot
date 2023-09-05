96d6e866ce146394d11821e6611b0fe0267e25f1
Fvbvke <novoselcevila12@gmail.com>
2023-09-05 07:55:43 +0300

    Full refactor
    I have created Detector class to avoid code reuse and loading\converting files too much times

--- utils/detecter.py
+++ utils/detecter.py
@@ -3,33 +3,34 @@ import numpy as np
 import subprocess
 from config import CONFIG
 
-# TODO: make singleton class for opening files with librosa.load
-# add method reset() to set instance to None
-#  save file name in class variables
-keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#',  'G', 'G#','A', 'A#', 'B']
 
-async def detect_bpm(filepath: str):
-    converted_path = f"{filepath.split('.')[0]}.mp3"
-    subprocess.run([CONFIG.ffmpeg_path, "-i", filepath, "-vn", "-ar", "44100", "-ac", "2", "-b:a", "192k", converted_path])
-    y, sr = librosa.load(converted_path, sr=None)
-    bpm = 0
-    if y.size != 0:
-        bpm = librosa.beat.tempo(y=y, sr=sr) # librosa.feature.rhythm.tempo  in future
+class Detecter:
+    def __init__(self, filepath):
+        self.keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#',  'G', 'G#','A', 'A#', 'B']
+        self.orig_path = filepath
+        self.converted_path = f"{filepath.split('.')[0]}.mp3"
 
-    return bpm
+        subprocess.run(
+            [CONFIG.ffmpeg_path, "-i", self.orig_path, "-vn", "-ar", "44100", "-ac",
+             "2", "-b:a", "192k", self.converted_path])
 
+        self.loaded_audio = librosa.load(self.converted_path, sr=None)
 
-async def detect_key(filepath: str):
-    converted_path = f"{filepath.split('.')[0]}.mp3"
-    subprocess.run(
-        [CONFIG.ffmpeg_path, "-i", filepath, "-vn", "-ar", "44100", "-ac", "2",
-         "-b:a", "192k", converted_path])
-    y, sr = librosa.load(converted_path, sr=None)
-    key = None
+    async def detect_bpm(self):
+        y, sr = self.loaded_audio
+        bpm = 0
+        if y.size != 0:
+            bpm = librosa.beat.tempo(y=y, sr=sr) # librosa.feature.rhythm.tempo  in future
 
-    chroma = librosa.feature.chroma_cqt(y=y, sr=sr)
-    chroma_vals = np.sum(chroma, axis=1)
-    most_common_pc = np.argmax(chroma_vals)
-    key = keys[most_common_pc-7]
+        return bpm
 
-    return key
+    async def detect_key(self):
+        y, sr = self.loaded_audio
+        key = None
+
+        chroma = librosa.feature.chroma_cqt(y=y, sr=sr)
+        chroma_vals = np.sum(chroma, axis=1)
+        most_common_pc = np.argmax(chroma_vals)
+        key = self.keys[most_common_pc - 7]
+
+        return key
