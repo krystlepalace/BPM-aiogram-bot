4d71355711ae6a00c36f62d3b933537c42419010
Fvbvke <novoselcevila12@gmail.com>
2023-09-07 20:55:14 +0300

    Fixed FSInput for thumbnail

--- handlers/tags.py
+++ handlers/tags.py
@@ -48,7 +48,7 @@ async def process_result(message: Message, data: Dict[str, Any]):
 
     await main.bot.send_audio(chat_id=message.from_user.id,
                               audio=FSInputFile(song_on_disk.__str__()),
-                              thumbnail=FSInputFile(song_on_disk.__str__()))
+                              thumbnail=FSInputFile(cover_on_disk.__str__()))
 
     os.remove(song_on_disk)
     os.remove(cover_on_disk)
